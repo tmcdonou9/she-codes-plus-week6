e797954fe46f3e28afba8abd21ccdb7a0b32d965
tmcdonou9 <110191044+tmcdonou9@users.noreply.github.com>
2022-08-11 11:40:22 -0400

    updated classes & added conversion function

--- index.js
+++ index.js
@@ -1,46 +1,48 @@
 // START OF DATE FORMAT
-let now = new Date();
+function formatDate(timestamp) {
+  let now = new Date();
 
-let currently = document.querySelector("#last-updated");
+  let currently = document.querySelector("#last-updated");
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-let day = days[now.getDay()];
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-let month = months[now.getMonth()];
-let date = now.getDate();
-let year = now.getFullYear();
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
+  let day = days[now.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+  let date = now.getDate();
+  let year = now.getFullYear();
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  currently.innerHTML = `Last updated on ${day}, ${month} ${date}, ${year} at ${hours}:${minutes}`;
 }
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-
-currently.innerHTML = `Last updated on ${day}, ${month} ${date}, ${year} at ${hours}:${minutes}`;
 //END OF DATE FORMATE
 
 // START FUNCTION TO SHOW CURRENT WEATHER INFO
@@ -50,11 +52,13 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#current-description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind-speed");
+  let dateElement = document.querySelector("#last-updated");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = response.data.wind.speed;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 // END FUNCTION TO SHOW CURRENT WEATHER INFO
 
@@ -98,14 +102,41 @@ function findCurrentLocation(event) {
   // let searchInput = document.querySelector();
   // axios.get(apiUrl).then(getCoordinates);
 }
-
+//END FUNCTION TO FIND CURRENT LOCATION??
+//START FUNCTION TO SHOW IMPERIAL UNITS
+function displayImperialTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#current-temperature");
+  metricButton.classList.remove("active");
+  imperialButton.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+//END FUNCTION TO SHOW IMPERIAL UNITS
+//START FUNCTION TO SHOW METRIC UNITS
+function displayMetricTemperature(event) {
+  event.preventDefault();
+  metricButton.classList.add("active");
+  imperialButton.classList.remove("active");
+  let temperatureElement = document.querySelector("#current-temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+//END FUNCTION TO SHOW METRIC UNITS
 //Function to allow "Search" interaction
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchPressEnter);
-//END FUNCTION TO FIND CURRENT LOCATION??
+
+let celsiusTemperature = null;
+
 //UHHH NOT SURE AT THE MOMENT FOR THIS...???
 let currentLocationButton = document.querySelector("#curr-loc-but");
 currentLocationButton.addEventListener("click", findCurrentLocation);
+
+let metricButton = document.querySelector("#metric-button");
+metricButton.addEventListener("click", displayMetricTemperature);
+
+let imperialButton = document.querySelector("#imperial-button");
+imperialButton.addEventListener("click", displayImperialTemperature);
 //
 //
 //
